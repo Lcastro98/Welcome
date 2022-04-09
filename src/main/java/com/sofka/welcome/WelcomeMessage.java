612d53ee3be345057af6973e11091fea83d9aac7
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-09 15:50:02 -0500

    Se documenta el código

--- src/main/java/com/sofka/welcome/WelcomeMessage.java
+++ src/main/java/com/sofka/welcome/WelcomeMessage.java
@@ -2,7 +2,22 @@ package com.sofka.welcome;
 
 import java.util.Scanner;
 
+/**
+ * Clase inicial del sistema Mensaje de bienvenida
+ *
+ * @version 1.0.0 2022-04-09
+ * @author Lorena Castro <Lcastro0398@gmail.com>
+ * @since 1.0.0
+ */
 public class WelcomeMessage {
+
+    /**
+     * Método principal con el que inicia el sistema
+     *
+     * @param args
+     * @author Lorena Castro <Lcastro0398@gmail.com>
+     * @since 1.0.0
+     */
     public static void main(String[] args) {
         Scanner capture = new Scanner(System.in);
         System.out.println("Nombre de usuario: ");
@@ -12,6 +27,6 @@ public class WelcomeMessage {
         System.out.println("Edad: ");
         int age = capture.nextInt();
         System.out.println("Bienvenido señor " + user + ", es un placer para nosotros contar con una persona de "
-                        + age + " años.\nPróximamente nos comunicaremos con usted al numero " + phone + ".\nFeliz día.");
+                        + age + " años.\nPróximamente nos comunicaremos con usted al numero " + phone + ".\nFeliz día");
     }
 }
